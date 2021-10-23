d6d6e6584b28a69cf4b073d118512ff7d67eb2d9
Ashis Bhowmik <ashisbhowmikisp@gmail.com>
2021-10-23 15:23:19 +0530

    custom splash screen added

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 // @dart=2.9
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:walpify/views/home.dart';
+import 'package:walpify/widget/widget.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,8 +15,42 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      showSemanticsDebugger: false,
-      home: Home(),
+      home: MyHomePage(),
     );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  void initState() {
+    Timer(Duration(seconds: 2), GotoHomePage);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(235, 234, 235, 2),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(120),
+          child: Container(
+            decoration: BoxDecoration(
+                image: DecorationImage(
+              image: AssetImage('assets/image/splash_logo.png'),
+            )),
+          ),
+        ),
+      ),
+    );
+  }
+
+  void GotoHomePage() {
+    Navigator.push(context, MaterialPageRoute(builder: (context) => Home()));
   }
 }
